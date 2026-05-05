dfadd3f98e3cf333b4f108df20db09e462e10594
Shweta Rani <shwetarani96936@gmail.com>
2026-05-05 17:29:20 +0530

    fix port for deployment

--- app.js
+++ app.js
@@ -130,8 +130,13 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error.ejs", { message });
 });
 
-// app.listen 
-app.listen(8080, () => {
-    console.log("server is listening to port 8080");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`);
 });
+// // app.listen 
+// app.listen(8080, () => {
+//     console.log("server is listening to port 8080");
+// });
 
